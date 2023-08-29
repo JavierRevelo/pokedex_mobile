22049100eafb76fefbb84db75be34a78d2154271
Javier Revelo <shavi.sprint@gmail.com>
2023-08-29 00:33:58 -0500

    carga de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
 
@@ -13,20 +13,65 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Icon(size: 200, Icons.person),
               TextFormField(
-                //controller: _emailController,
                 decoration: const InputDecoration(labelText: 'Usuario'),
               ),
               TextFormField(
-                //controller: _passwordController,
                 decoration: const  InputDecoration(labelText: 'Clave'),
                 obscureText: true,
               ),
               ElevatedButton(
                 onPressed: (){},
+                child: const Text('Iniciar Sesión'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pokedex_mobile/providers/login_provider.dart'; // Importa el provider de inicio de sesión
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = 'pokemon-login';
+  
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final loginProvider = Provider.of<AuthenticationRepository>(context); // Obtén una instancia del LoginProvider
+
+    // Controladores para los campos de usuario y contraseña
+    final TextEditingController userController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pokedex Login'),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const Icon(size: 200, Icons.person),
+              TextFormField(
+                controller: userController, // Asigna el controlador al campo de usuario
+                decoration: const InputDecoration(labelText: 'Usuario'),
+              ),
+              TextFormField(
+                controller: passwordController, // Asigna el controlador al campo de contraseña
+                decoration: const  InputDecoration(labelText: 'Clave'),
+                obscureText: true,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Aquí puedes llamar al método de inicio de sesión del provider
+                  loginProvider.logInWithEmailAndPassword(email: userController.text, password: passwordController.text );
+                },
                 child: const Text('Iniciar Sesión'),
               ),
             ],
